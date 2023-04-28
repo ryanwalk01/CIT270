b4a97ac7c2960fbe118f7f6cc41317e7b54a06b4
ryanwalk01 <ryan.walk01@gmail.com>
2023-04-28 14:08:52 -0600

    Added login command

--- login.cmd
+++ login.cmd
@@ -1,2 +1,3 @@
 echo "logging in"
-curl https://dev.stedi.me/validate/f1018dff-7015-493e-823e-04d8dabf0094
+curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
+curl -v https://dev.stedi.me/validate/f1018dff-7015-493e-823e-04d8dabf0094
