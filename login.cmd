caf0c8ffbb9f4c396578a31c0043e8e6b9ef84b5
ryanwalk01 <ryan.walk01@gmail.com>
2023-05-12 13:14:08 -0600

    Added password checking.

--- login.cmd
+++ login.cmd
@@ -1,3 +1,3 @@
 echo "logging in"
-curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
-curl -v https://dev.stedi.me/validate/f1018dff-7015-493e-823e-04d8dabf0094
+curl -k -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+@REM curl -v https://dev.stedi.me/validate/f1018dff-7015-493e-823e-04d8dabf0094
--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const Redis = require("redis")
+const redisClient = Redis.createClient();
 
 const port = 3000;
 app.listen(port, ()=> {
+    redisClient.connect();
     console.log("Listening on port: " + port)
 });
+
+app.use(bodyParser.json());
 
 app.get('/',(req,res) => {
     res.send("Welcome to Ryan's Node Server! :)")
     // res.redirect("http://google.com")
 })
+
+app.post("/login", (req,res) => {
+    const loginBody = req.body;
+    const userName = loginBody.userName;
+    const password = loginBody.password;
+    if (password === "P@ssw0r") {
+        res.send("welcome "+ userName);
+    }
+    else {
+        res.status(401); //unathorized
+        res.send("Incorrect Password");
+    }
+});
