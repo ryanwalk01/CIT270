b6b085aeb59b9760e1201ebc66e5aa26abf7f135
ryanwalk01 <ryan.walk01@gmail.com>
2023-06-19 12:46:30 -0600

    Changed login script to own server

--- login.cmd
+++ login.cmd
@@ -1,3 +1,3 @@
 echo "logging in"
-curl -k -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+curl -k -v -d "@login.json" POST -H "Content-Type:application/json" https://ryanwalker.cit270.com/login
 @REM curl -v https://dev.stedi.me/validate/f1018dff-7015-493e-823e-04d8dabf0094
