2c8800a055fcf26f6387ee05262b4fa2193663e6
Ryan Walker <ryan.walk01@gmail.com>
2023-07-10 14:52:09 -0600

    Reverted change

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Redis = require("redis")
-const redisClient = Redis.createClient({
-    host: 'localhost',
-    port: 6379,
-    password: 'secret',
-    db: 0
-  });
+const redisClient = Redis.createClient({url:'redis://default@redis-stedi-ryan:6379'});
 const {createHash} = require('node:crypto');
 const https = require('https');
 const fs = require('fs');
