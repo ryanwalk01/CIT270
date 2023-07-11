8605596d65bdcded25eda30f52106ca29db4ddfd
Ryan Walker <ryan.walk01@gmail.com>
2023-07-10 18:32:10 -0600

    Reverted rewritten code

--- server.js
+++ server.js
@@ -20,22 +20,11 @@ const port = 3000;
 //     redisClient.connect();
 //     console.log('Listening...')
 //   });
-
-redisClient.on("connect", () => {
-    console.log("Connected to Redis server");
-    startServer();
-  });
   
-  redisClient.on("error", (err) => {
-    console.log("Redis Server Error", err);
-  });
-  
-async function startServer() {
-  app.use(bodyParser.json()); // Allow JSON (Javascript Object Notation) requests
-
-  app.get("/", (req, res) => {
-    res.send("Welcome to Ryan's Node Server!");
-  });
+app.listen(port, ()=> {
+    redisClient.connect(); //the API Sever is trying to connect with Redis
+    console.log("Listening on port: " + port);
+});
 
 app.use(bodyParser.json()); //allow JSON (Javascript Object Notation) requests
 
@@ -44,24 +33,20 @@ app.get('/',(req,res) => {
     // res.redirect("http://google.com")
 })
 
-    app.post("/login", async (req,res) => {
-        const loginBody = req.body;
-        const userName = loginBody.userName;
-        const password = loginBody.password;
-        const hashedPassword = passsword = null ? null: createHash('sha3-256').update(password).digest('hex');
-        console.log("Hashed Password: " + hashedPassword);
-        const redisPassword = passsword = null ? null: await redisClient.hGet('hashedpasswords',userName);
-        console.log("Password for " + userName + " " + redisPassword);
-        if (password != null && hashedPassword == redisPassword) {
-            
-            res.send("Welcome "+ userName);
-        }
-        else {
-            res.status(401); //unathorized
-            res.send("Incorrect Password");
-        }
-    });
-    app.listen(port, () => {
-        console.log("Listening on port: " + port);
-      });
-}
+app.post("/login", async (req,res) => {
+    const loginBody = req.body;
+    const userName = loginBody.userName;
+    const password = loginBody.password;
+    const hashedPassword = passsword = null ? null: createHash('sha3-256').update(password).digest('hex');
+    console.log("Hashed Password: " + hashedPassword);
+    const redisPassword = passsword = null ? null: await redisClient.hGet('hashedpasswords',userName);
+    console.log("Password for " + userName + " " + redisPassword);
+    if (password != null && hashedPassword == redisPassword) {
+        
+        res.send("Welcome "+ userName);
+    }
+    else {
+        res.status(401); //unathorized
+        res.send("Incorrect Password");
+    }
+});
